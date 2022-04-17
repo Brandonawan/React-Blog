1fe6862583aaf88ea31fc4c4cc0141abbeb48225
Brandon awan <abibangbrandon855@gmail.com>
2022-04-17 08:13:04 -0700

    import navbar.js to app.js

--- brandon-blog/src/App.js
+++ brandon-blog/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
-  const title = 'Welcome to the new blog'
-  const likes = 50;
-  const myarr = [1,2,3,4];
-  const cars = ["Saab", "Volvo", "BMW"];
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-      <h1>{title}</h1>
       <h2>App Component</h2>
-      <p>Liked {likes} times</p>
-      <p>{"hello ninjas"}</p>
-      <p>{myarr}</p>
-      <p>{cars}</p>
-      <p>{Math.random * 10}</p>
       </div>
     </div>
   );
